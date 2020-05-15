f06dbb9a59f133ad9ebf9c620365462bf96e88a0
desiska <desislava_branimirova@abv.bg>
2020-05-15 06:16:46 +0300

    Validation
    The Apiary and Beehive Entities are validated.

--- Bees Diary/Database/Entities/Apiary.cs
+++ Bees Diary/Database/Entities/Apiary.cs
@@ -1,25 +1,191 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Database.Entities
 {
     public class Apiary
     {
+        private string id;
+        private string name;
+        private string type;
+        private string location;
+        private decimal power;
+        private decimal production;
+        private ICollection<Beehive> beehives;
+        private ICollection<string> plantsInArea;
+
         public Apiary()
         {
             this.Beehives = new HashSet<Beehive>();
             this.PlantsInArea = new List<string>();
         }
 
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public int Power { get; set; }
+        public Apiary(string name, string type, string location, DateTime creationTime)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Location = location;
+            this.CreationTime = creationTime;
+        }
 
-        public int Production { get; set; }
-        public string Location { get; set; }
-        public virtual ICollection<Beehive> Beehives { get; set; }
-        public ICollection<string> PlantsInArea { get; set; }
+        public Apiary(string name, string type, string location, DateTime creationTime, ICollection<Beehive> beehives)
+            :this(name, type, location, creationTime)
+        {
+            this.Beehives = beehives;
+        }
 
+        public Apiary(string name, string type, string location, DateTime creationTime, decimal production)
+            :this(name, type, location, creationTime)
+        {
+            this.Production = production;
+        }
 
+        public Apiary(string name, string type, string location, DateTime creationTime, decimal production, ICollection<Beehive> beehives)
+            : this(name, type, location, creationTime, production)
+        {
+            this.beehives = beehives;
+        }
+
+        public string ID 
+        {
+            get
+            {
+                return this.id;
+            }
+            private set
+            {
+                this.id = new Guid().ToString();
+            }
+        }
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if(string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("The name cannot contain a whitespace or to be empty");
+                }
+                else if(value.Length < 5 || value.Length > 45)
+                {
+                    throw new Exception("The name's length cannot be less than 5 symbols and more than 45 symbols");
+                }
+                else
+                {
+                    this.name = value;
+                }
+            } 
+        }
+        public string Type 
+        {
+            get
+            {
+                return this.type;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("The type cannot contain a whitespace or to be empty");
+                }
+                else if (value.Length < 5 || value.Length > 45)
+                {
+                    throw new Exception("The type's length cannot be less than 5 symbols and more than 45 symbols");
+                }
+                else
+                {
+                    this.type = value;
+                }
+            }
+        }
+        public string Location
+        {
+            get
+            {
+                return this.location;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("The location cannot contain a whitespace or to be empty");
+                }
+                else if (value.Length < 5 || value.Length > 45)
+                {
+                    throw new Exception("The location's length cannot be less than 5 symbols and more than 45 symbols");
+                }
+                else
+                {
+                    this.location = value;
+                }
+            } 
+        }
+        public decimal Power 
+        {
+            get
+            {
+                return this.power;
+            }
+            private set
+            {
+                //We have to think about a formula
+            }
+        }
+        public decimal Production 
+        {
+            get
+            {
+                return this.production;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("The production cannot be less than zero");
+                }
+                else
+                {
+                    this.production = value;
+                }
+            }
+        }
+        public DateTime CreationTime { get; set; }
+        public virtual ICollection<Beehive> Beehives 
+        {
+            get
+            {
+                return this.beehives;
+            }
+            set
+            {
+                this.beehives = value;
+            }
+        }
+        public ICollection<string> PlantsInArea 
+        {
+            get
+            {
+                return this.plantsInArea;
+            }
+            set
+            {
+                foreach (var item in value)
+                {
+                    if (string.IsNullOrEmpty(item) || string.IsNullOrWhiteSpace(item))
+                    {
+                        throw new Exception("The name cannot contain a whitespace or to be empty");
+                    }
+                    else if (item.Length < 5 || item.Length > 45)
+                    {
+                        throw new Exception("The name's length cannot be less than 5 symbols and more than 45 symbols");
+                    }
+                }
+
+                this.plantsInArea = value;
+            }
+        }
     }
 }
--- Bees Diary/Database/Entities/Beehive.cs
+++ Bees Diary/Database/Entities/Beehive.cs
@@ -6,17 +6,101 @@ namespace Database.Entities
 {
     public class Beehive
     {
+        private string id;
+        private string name;
+        private string typeBeehive;
+        private string typeBees;
         public Beehive()
         {
 
         }
 
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string TypeBeehive { get; set; }
-        public string TypeBees { get; set; }
-        public int ApiaryID { get; set; }
+        public Beehive(string name, string typeBeehive, string typeBees, int apiaryID)
+        {
+            this.Name = name;
+            this.TypeBeehive = typeBeehive;
+            this.TypeBees = typeBees;
+            this.ApiaryID = apiaryID;
+        }
 
+        public string ID 
+        {
+            get
+            {
+                return this.id;
+            }
+            private set
+            {
+                this.id = new Guid().ToString();
+            }
+        }
+        public string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("The name cannot contain a whitespace or to be empty");
+                }
+                else if (value.Length < 5 || value.Length > 45)
+                {
+                    throw new Exception("The name's length cannot be less than 5 symbols and more than 45 symbols");
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+        }
+        public string TypeBeehive 
+        {
+            get
+            {
+                return this.typeBeehive;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("The type of beehive cannot contain a whitespace or to be empty");
+                }
+                else if (value.Length < 5 || value.Length > 45)
+                {
+                    throw new Exception("The type's length cannot be less than 5 symbols and more than 45 symbols");
+                }
+                else
+                {
+                    this.typeBeehive = value;
+                }
+            }
+        }
+        public string TypeBees 
+        {
+            get
+            {
+                return this.typeBees;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("The type of bees cannot contain a whitespace or to be empty");
+                }
+                else if (value.Length < 5 || value.Length > 45)
+                {
+                    throw new Exception("The type's length cannot be less than 5 symbols and more than 45 symbols");
+                }
+                else
+                {
+                    this.typeBees = value;
+                }
+            }
+        }
+        public int ApiaryID { get; set; }
         public virtual Apiary Apiary { get; set; }
     }
 }
